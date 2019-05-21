1af968521a9944c2c3730a5f57f77eb620670335
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-05-21 14:31:03 +0300

    Some fixes

---
+++ src/View.php
@@ -0,0 +1,132 @@
+<?php
+
+/**
+ * The PHP View Engine.
+ *
+ * @package dionchaika/view
+ * @version 1.0.0
+ * @license MIT
+ * @author Dion Chaika <dionchaika@gmail.com>
+ */
+
+namespace Dionchaika\View;
+
+use RuntimeException;
+use InvalidArgumentException;
+
+class View
+{
+    /**
+     * The views directory.
+     *
+     * @var string
+     */
+    protected $viewsDir;
+
+    /**
+     * The compiled views directory.
+     *
+     * @var string
+     */
+    protected $compiledViewsDir;
+
+    /**
+     * @param string $viewsDir
+     * @param string $compiledViewsDir
+     */
+    public function __construct(string $viewsDir, string $compiledViewsDir)
+    {
+        $this->viewsDir = rtrim($viewsDir, '/');
+        $this->compiledViewsDir = rtrim($compiledViewsDir, '/');
+    }
+
+    /**
+     * Get the views directory.
+     *
+     * @return string
+     */
+    public function getViewsDir(): string
+    {
+        return $this->viewsDir;
+    }
+
+    /**
+     * Get the compiled views directory.
+     *
+     * @return string
+     */
+    public function getCompiledViewsDir(): string
+    {
+        return $this->compiledViewsDir;
+    }
+
+    /**
+     * Render view into HTML.
+     *
+     * @param string  $viewName
+     * @param mixed[] $viewParameters
+     * @return string
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
+    public function render(string $viewName, array $viewParameters = []): string
+    {
+        
+    }
+
+    /**
+     * Compile view into PHP.
+     *
+     * @param string $viewPath
+     * @return string
+     * @throws \RuntimeException
+     */
+    public function compile(string $viewPath): string
+    {
+        $view = @file_get_contents($viewPath);
+        if (false === $view) {
+            throw new RuntimeException(
+                'Unable to get the contents of the file: '.$viewPath.'!'
+            );
+        }
+
+        $view = $this->compilePlaceholders($view);
+        $view = $this->compilePhpDirective($view);
+        $view = $this->compileViewDirective($view);
+
+        return $view;
+    }
+
+    /**
+     * Compile view placeholders.
+     *
+     * @param string $view
+     * @return string
+     */
+    protected function compilePlaceholders(string $view): string
+    {
+        return preg_replace('/\{\{\s*([^\s]+)\s*\}\}/', "<?=$1?>", $view);
+    }
+
+    /**
+     * Compile @php directive.
+     *
+     * @param string $view
+     * @return string
+     */
+    protected function compilePhpDirective(string $view): string
+    {
+        return preg_replace('/\@php([^@]+)\@end/', "<?php\n$1\n?>", $view);
+    }
+
+    /**
+     * Compile @view directive.
+     *
+     * @param string $view
+     * @return string
+     */
+    protected function compileViewDirective(string $view): string
+    {
+        return preg_replace('/\@view +([\w.]+)/', "<?=\$this->render('$1')?>", $view);
+    }
+}
